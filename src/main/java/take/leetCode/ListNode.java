8a50d71dd3f1f927180fdd72efec9126af921c66
userEO <39209187+userEO@users.noreply.github.com>
2019-11-21 16:10:44 +0800

    1 两数之和 2 两数相加 3 无重复字符的最长子串

---
+++ src/main/java/take/leetCode/ListNode.java
@@ -0,0 +1,13 @@
+package take.leetCode;
+
+/**
+ * 功能描述：
+ *
+ * @author EO
+ * @date 2019/11/21 16:08
+ */
+public class ListNode {
+    int val;
+      ListNode next;
+     ListNode(int x) { val = x; }
+}
