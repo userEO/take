e215096a666b109ff1421be0459a2c63a10f27f0
“EO” <18756940319@163.com>
2021-05-19 20:05:57 +0800

    EO timer类测试

--- src/main/java/take/frame/TimerUtil.java
+++ src/main/java/take/frame/TimerUtil.java
@@ -6,6 +6,7 @@ import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 功能描述：
@@ -16,9 +17,13 @@ import java.util.concurrent.ScheduledThreadPoolExecutor;
 public class TimerUtil {
     private static Timer timer = new Timer();
    private static SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-    // 核心线程为1
-    private static ScheduledExecutorService schedule = new ScheduledThreadPoolExecutor(1);
+    // 核心线程为2 如果核心为1那么就和timer一样了
+    private static ScheduledExecutorService schedule = new ScheduledThreadPoolExecutor(2);
     public static void main(String[] args) {
+        one();
+        two();
+        three();
+        four();
     }
     private static void one(){
         timer.schedule(new TimerTask() {
@@ -30,7 +35,7 @@ public class TimerUtil {
                 }catch (Exception e){
                     e.printStackTrace();
                 }
-                System.out.println("当前线程一执行"+simpleDateFormat.format(new Date()));
+                System.out.println("当前一执行"+simpleDateFormat.format(new Date()));
             }
         },5000);
 
@@ -39,9 +44,36 @@ public class TimerUtil {
         timer.schedule(new TimerTask() {
             @Override
             public void run() {
-                System.out.println("当前线程一执行"+simpleDateFormat.format(new Date()));
+                System.out.println("当前二执行"+simpleDateFormat.format(new Date()));
             }
         },5000);
-
+    }
+    private static void three(){
+        schedule.schedule(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    // ScheduledExecutorService不会阻挡第四个执行
+                    Thread.sleep(3000);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+                System.out.println("当前三执行"+simpleDateFormat.format(new Date()));
+            }
+        }, 3000, TimeUnit.MICROSECONDS);
+    }
+    private static void four(){
+        schedule.schedule(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    // ScheduledExecutorService不会阻挡第四个执行
+                    Thread.sleep(3000);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+                System.out.println("当前四执行"+simpleDateFormat.format(new Date()));
+            }
+        }, 3000, TimeUnit.MICROSECONDS);
     }
 }
